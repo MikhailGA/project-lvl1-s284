2d8e6a3e1c1574a401b6264b9c4f3a8e290d96dd
FreonMG <freon2006@bkru>
2018-06-02 15:58:23 +0300

    Update algoritm prime.js

--- src/game/prime.js
+++ src/game/prime.js
@@ -1,43 +1,25 @@
 import runGame from '../game-engine';
 import getRandom from '../utils';
 
-const makeResheto = (number) => {
-  const n = number + 1;
-  // Eratosthenes algorithm to find all primes under n
-  const array = [];
-  const upperLimit = Math.sqrt(n);
-  const output = [2];
-  // Make an array from 2 to (n - 1)
-  for (let i = 0; i < n; i += 1) {
-    array.push(1);
-  }
-  // Remove multiples of primes starting from 2, 3, 5,...
-  for (let i = 3; i <= upperLimit; i += 2) {
-    if (array[i]) {
-      for (let j = i * i; j < n; j += i * 2) {
-        array[j] = 0;
-      }
-    }
-  }
-  // All array[i] set to 1 (true) are primes
-  for (let i = 3; i < n; i += 2) {
-    if (array[i]) {
-      output.push(i);
-    }
-  }
-  return output;
-};
-
 const gameTask = 'Is this number prime?';
 
-const upperLimit = 150;
+const isPrime = (number) => {
+  const iter = (divisor) => {
+    if (number % divisor !== 0) {
+      return iter(divisor + 1);
+    }
 
-const reshetoEratosfena = makeResheto(upperLimit);
+    if (number % divisor === 0 && divisor === number) {
+      return true;
+    }
+    return false;
+  };
 
-const isPrime = number => reshetoEratosfena.includes(number);
+  return iter(2);
+};
 
 const gameData = () => {
-  const a = getRandom(2, upperLimit);
+  const a = getRandom(2, 150);
 
   const question = a;
   const answer = isPrime(a) ? 'yes' : 'no';
